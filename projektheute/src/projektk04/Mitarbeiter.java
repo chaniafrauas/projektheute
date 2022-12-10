40a8dfac64351ef4064f51ea8dbb2cf979965bbc
Raphael <Raphael@RDK>
2022-12-10 13:42:42 +0100

    projekt commit

---
+++ projektheute/src/projektk04/Mitarbeiter.java
@@ -0,0 +1,33 @@
+package projektk04;
+
+public class Mitarbeiter {
+
+	private String nameMitarbeiter;
+
+	private String anrede;
+
+	private int position;
+
+	private int telefon;
+
+	public void loeschenMitarbeiter() {
+
+	}
+
+	public void setMitarbeiterName() {
+
+	}
+
+	public void getMitarbeiterName() {
+
+	}
+
+	public void setServiceAuftrag() {
+
+	}
+
+	public void getServiceAuftrag() {
+
+	}
+
+}
